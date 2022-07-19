2b483046dd3c60acc5b8f3839d014a284c27c3c5
Kurt Maglalang <kbm126@live.com>
2022-07-18 18:52:10 -0700

    more setup for the navigation bar

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -7,15 +7,24 @@ export default function NavigationBar() {
   return (
     <Fragment>
       <header className={styles.header}>
-        <div className="header-container">
-          <Link href="/" className={styles.logo}>
-            Golf Now
-          </Link>
+        <div className={styles.logo}>
+          <Link href="/">Golf Now</Link>
+        </div>
+        <div className="search-input">
+          <input type="text" placeholder="Search..."></input>
+          <button
+            type="button"
+            width="300px"
+            className="btn"
+            onClick={() => {}}
+          >
+            Search
+          </button>
+        </div>
+        <nav>
           <ul>
             <li>
-              <Link href="/login" className="header-link">
-                Log In
-              </Link>
+              <Link href="/login">Log In</Link>
             </li>
             <li>
               <Link href="/cart">
@@ -23,20 +32,42 @@ export default function NavigationBar() {
               </Link>
             </li>
           </ul>
-        </div>
+        </nav>
       </header>
-      <div className="navigation">
-        <nav>
-          <Link href="/new">New</Link>
-          <Link href="/clubs">Clubs</Link>
-          <Link href="/balls">Balls</Link>
-          <Link href="/shoes">Shoes</Link>
-          <Link href="/clothing">Clothing</Link>
-          <Link href="/bag-carts">Bags & Carts</Link>
-          <Link href="/golf-tech">Golf Tech</Link>
-          <Link href="/accessories">Accessories</Link>
-          <Link href="/brands">Brands</Link>
-          <Link href="/golf-deals">Golf Deals</Link>
+      <div>
+        <nav className={styles.navigation}>
+          <ul>
+            <li>
+              <Link href="/new">New</Link>
+            </li>
+            <li>
+              <Link href="/clubs">Clubs</Link>
+            </li>
+            <li>
+              <Link href="/balls">Balls</Link>
+            </li>
+            <li>
+              <Link href="/shoes">Shoes</Link>
+            </li>
+            <li>
+              <Link href="/clothing">Clothing</Link>
+            </li>
+            <li>
+              <Link href="/bag-carts">Bags & Carts</Link>
+            </li>
+            <li>
+              <Link href="/golf-tech">Golf Tech</Link>
+            </li>
+            <li>
+              <Link href="/accessories">Accessories</Link>
+            </li>
+            <li>
+              <Link href="/brands">Brands</Link>
+            </li>
+            <li>
+              <Link href="/golf-deals">Golf Deals</Link>
+            </li>
+          </ul>
         </nav>
       </div>
     </Fragment>
