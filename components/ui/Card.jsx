30e6ec681dd7951c51f54232e2e288d873e1b856
Kurt Maglalang <kbm126@live.com>
2022-07-18 20:25:21 -0700

    initial card component

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import styles from "./Card.module.css";
 
 export default function Card(props) {
-  return <div>Card</div>;
+  return <div className={styles.card}>{props.children}</div>;
 }
