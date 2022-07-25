a25711aaa29ecee0dd57a94c4a16a003b9d0326f
Kurt Maglalang <kbm126@live.com>
2022-07-24 23:12:55 -0700

    add new pages related to footer

---
+++ pages/accessibility.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Accesibility() {
+  return <div>Accesibility</div>;
+}
---
+++ pages/policy.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Policy() {
+  return <div>Policy</div>;
+}
