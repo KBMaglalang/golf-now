cb3bda468176ba38978ec548b6039e23c4aec33d
Kurt Maglalang <kbm126@live.com>
2022-07-24 22:11:45 -0700

    update layout with footer

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import NavigationBar from "./NavigationBar";
-import Footer from "./Footer";
+import FooterBar from "./Footer";
 import styles from "./Layout.module.css";
 
 export default function Layout(props) {
   return (
-    <div>
+    <Fragment>
       <NavigationBar />
       <main className={styles.main}>{props.children}</main>
-      <Footer />
-    </div>
+      <footer className={styles.footer}>
+        <FooterBar />
+      </footer>
+    </Fragment>
   );
 }
