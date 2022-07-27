a218bf0167fed19b00a1a9934c395c7d6302ef3b
Kurt Maglalang <kbm126@live.com>
2022-07-26 20:55:15 -0700

    initial plan for products category

--- pages/clubs/index.jsx
+++ pages/clubs/index.jsx
@@ -1,5 +1,50 @@
 import React from "react";
+import Banner from "../../components/ui/Banner";
+import Card from "../../components/ui/Card";
+import Image from "next/image";
 
 export default function ClubsBase() {
-  return <div>CLUBS</div>;
+  return (
+    <div>
+      <Banner />
+      <h1>Golf Clubs</h1>
+      <div>Filter Bar</div>
+      <div className="products-container">
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+        <Card>
+          <Image src="/" alt="pic" width={500} height={500} />
+          <h1>top product</h1>
+        </Card>
+      </div>
+      <div className="btn-container">
+        <p>Showiing # out of #</p>
+        <button type="button" className="btn" onClick="">
+          Load More
+        </button>
+      </div>
+    </div>
+  );
 }
