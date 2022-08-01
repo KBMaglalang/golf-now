e74d1cd53607d12162b78a48d70d427c3202d2c0
Kurt Maglalang <kbm126@live.com>
2022-08-01 12:03:27 -0700

    base fields for shoes

---
+++ golf-sanity-studio/schemas/shoes.js
@@ -0,0 +1,76 @@
+export default {
+  name: "shoes",
+  title: "Shoes",
+  type: "document",
+  fields: [
+    {
+      name: "brand",
+      title: "Brand",
+      type: "string",
+    },
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "sku",
+      title: "SKU",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "slug",
+      type: "slug",
+      options: {
+        source: `sku`,
+        maxLength: 80,
+      },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+    },
+    {
+      name: "stock",
+      title: "Stock",
+      type: "number",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+    {
+      name: "features",
+      title: "Features",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+  ],
+};
