b8bc807e82ef0b2093e338cb86c9224106030e7a
Kurt Maglalang <kbm126@live.com>
2022-08-11 18:19:38 -0700

    grab product category and list on page

--- pages/clubs/index.jsx
+++ pages/clubs/index.jsx
@@ -1,60 +1,66 @@
 import Banner from "../../components/ui/Banner";
 import Card from "../../components/ui/Card";
 import Image from "next/image";
-import { cmsClient, urlFor } from "../lib/sanityClient";
+import { cmsClient, urlFor } from "../../lib/sanityClient";
+import Head from "next/head";
+import styles from "../../styles/Product.module.css"; // ! could change this to another css
 
-export default function ClubsBase() {
+export default function ClubsBase({ products }) {
+  const listProducts = (products) => {
+    // filter down products into the card
+    return products
+      ?.filter((product) => product.stock > 0)
+      .map((product) => <Card key={product.sku} product={product} />);
+  };
+
+  const loadMoreProducts = () => {};
+
   return (
-    <div>
-      <Banner />
-      <h1>Golf Clubs</h1>
-      <div className="filter-container">
-        <h2>Filter</h2>
-        <p>Brand</p>
-        <p>Price</p>
-        <p>Size</p>
-        <p>Hand</p>
-        <p>Shaft</p>
-        <p>Flex</p>
-        <p>Loft</p>
-        <p>Wedge Head</p>
-      </div>
-      <div className="products-container">
-        {/* <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card>
-        <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card>
-        <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card>
-        <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card>
-        <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card>
-        <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card>
-        <Card>
-          <Image src="/" alt="pic" width={500} height={500} />
-          <h1>top product</h1>
-        </Card> */}
-      </div>
-      <div className="btn-container">
-        <p>Showing # out of #</p>
-        <button type="button" className="btn" onClick="">
-          Load More
-        </button>
-      </div>
+    <div className={styles.container}>
+      <Head>
+        <title>{`Golf Now`}</title>
+        <meta name="description" content="Golf Products" />
+        <link rel="icon" href="/golf-ball-icon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        {/* <Banner /> */}
+        <h1>Golf Clubs</h1>
+        {/* <div className="filter-container">
+          <h2>Filter</h2>
+          <p>Brand</p>
+          <p>Price</p>
+          <p>Size</p>
+          <p>Hand</p>
+          <p>Shaft</p>
+          <p>Flex</p>
+          <p>Loft</p>
+          <p>Wedge Head</p>
+        </div> */}
+        <div className="products-container">{listProducts(products)}</div>
+        <div className="btn-container">
+          <p>Showing # out of #</p>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => {
+              loadMoreProducts();
+            }}
+          >
+            Load More
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const products = await cmsClient.fetch(
+    '*[_type == "clubs"]{_type, slug, image, name, price, stock, sku, brand->{_id,title}}'
+  );
+
+  return {
+    props: { products },
+  };
+};
