b29d550d1ae28df7146edac4db810b18b170af1d
Kurt Maglalang <kbm126@live.com>
2022-08-18 16:28:31 -0700

    finish brandCard component

--- components/ui/BrandCard.jsx
+++ components/ui/BrandCard.jsx
@@ -1,4 +1,4 @@
-import styles from "./Card.module.css";
+import styles from "./BrandCard.module.css";
 import Link from "next/link";
 import { urlFor } from "../../lib/sanityClient";
 import Image from "next/image";
@@ -7,20 +7,20 @@ export default function BrandCard({ brand: { logo, _type, title } }) {
   console.log("🚀 ~ file: BrandCard.jsx ~ line 7 ~ BrandCard ~ logo", logo);
   return (
     <div className={styles.card}>
-      {/* <Link href={`/products/${_type}/${slug.current}`}> */}
-      {/* <a> */}
-      <div>
-        <img
-          src={urlFor(logo)}
-          width={"100%"}
-          height={"auto"}
-          object-fit={"contain"}
-          alt={`${title}`}
-        />
-      </div>
-      <h3>{title}</h3>
-      {/* </a> */}
-      {/* </Link> */}
+      <Link href={`/products/${_type}/`}>
+        <a>
+          <div>
+            <img
+              src={urlFor(logo)}
+              width={"100%"}
+              height={"auto"}
+              object-fit={"contain"}
+              alt={`${title}`}
+            />
+          </div>
+          <h3>{title}</h3>
+        </a>
+      </Link>
     </div>
   );
 }
