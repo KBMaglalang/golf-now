ce2a26307775aecff8d2d28b421346b83114e07c
Kurt Maglalang <kbm126@live.com>
2022-08-29 13:06:16 -0700

    add stripe checkout

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import { cmsClient, urlFor } from "../lib/sanityClient";
-import { PortableText } from "@portabletext/react";
 import { useStateContext } from "../context/StateContext";
 import CartItem from "../components/ui/CartItem";
+import getStripe from "../lib/stripe";
+import { cmsClient, urlFor } from "../lib/sanityClient";
+import { PortableText } from "@portabletext/react";
 
 export default function Cart() {
   const { cartItems, onRemove, toggleCartItemQuantity } = useStateContext();
@@ -15,6 +16,24 @@ export default function Cart() {
 
     const temp = items.map((e) => <CartItem key={e._id} product={e} />);
     return temp;
+  };
+
+  const handleCheckout = async () => {
+    toast.loading("Redirecting...");
+
+    // process the order with stripe
+    const stripe = await getStripe();
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ cartItems, user }),
+    });
+    if (response.statusCode === 500) return;
+    const data = await response.json();
+
+    stripe.redirectToCheckout({ sessionId: data.id });
   };
 
   //number of unique products in the cart
@@ -40,7 +59,7 @@ export default function Cart() {
         <p>sales tax</p>
         <p>estimated total</p>
         <div className="btn-container">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={handleCheckout}>
             Pay with Stripe
           </button>
         </div>
