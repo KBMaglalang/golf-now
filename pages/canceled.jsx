9d3ce6f6de398cc71b7ddbe05dc925284c6b4dbd
Kurt Maglalang <kbm126@live.com>
2022-08-29 17:08:23 -0700

    clean up cancel page

--- pages/canceled.jsx
+++ pages/canceled.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { AiOutlineQuestion } from "react-icons/ai";
 import { runFireworks } from "../lib/fireworks";
 
 const Canceled = () => {
@@ -12,12 +11,6 @@ const Canceled = () => {
     <div className="cancel-wrapper">
       <div className="cancel">
         <h2>Forgot to add something to your cart?</h2>
-        {/* <p className="description">
-          If you have any questions, please email
-          <a className="email" href="mailto:order@example.com">
-            OOgunremi@yahoo.com
-          </a>
-        </p> */}
         <Link href="/">
           <button type="button" width="300px" className="btn">
             Return Home
