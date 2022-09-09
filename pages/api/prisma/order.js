d791dbb2e5ce6d90c2329c1b28a487bd966f7cae
Kurt Maglalang <kbm126@live.com>
2022-09-08 19:48:24 -0700

    create order prisma test code

---
+++ pages/api/prisma/order.js
@@ -0,0 +1,61 @@
+import prisma from "../../../lib/prisma";
+
+export default async function handle(req, res) {
+  if (req.method === "POST") {
+    try {
+      const result = await prisma.order.create({
+        data: {
+          productSKU: "123",
+          quantity: 10,
+          userId: "cl7r8kaqb00064mhdkw5eifzc",
+        },
+      });
+      res.json(result);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else if (req.method === "GET") {
+    try {
+      const result = await prisma.order.findMany({
+        where: {
+          userId: "cl7r8kaqb00064mhdkw5eifzc",
+        },
+      });
+      console.log("🚀 ~ file: prisma.js ~ line 42 ~ handle ~ result", result);
+      res.json(result);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else if (req.method === "PUT") {
+    try {
+      const result = await prisma.order.update({
+        where: {
+          userId: "cl7r8kaqb00064mhdkw5eifzc",
+        },
+        data: {
+          orderStatus: "cancelled",
+        },
+      });
+      console.log("🚀 ~ file: prisma.js ~ line 57 ~ handle ~ result", result);
+      res.json(result);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else if (req.method === "DELETE") {
+    console.log("in delete route");
+    try {
+      const result = await prisma.order.delete({
+        where: {
+          userId: "cl7r8kaqb00064mhdkw5eifzc",
+        },
+      });
+      console.log("🚀 ~ file: prisma.js ~ line 71 ~ handle ~ result", result);
+      res.json(result);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
