d8f10d19e0f8ab15d0d1db3d4dd8422b625df9f8
Kurt Maglalang <kbm126@live.com>
2022-09-13 16:52:09 -0700

    testing other stripe api endpoints

--- pages/api/stripe/balanceTransactions.js
+++ pages/api/stripe/balanceTransactions.js
@@ -11,6 +11,18 @@ export default async function handler(req, res) {
     }
   } else if (req.method === "GET") {
     try {
+      console.log(
+        "🚀 ~ file: balanceTransactions.js ~ line 13 ~ handler ~ req.",
+        req.query
+      );
+      // const session = await stripe.checkout.sessions.retrieve(req.query.key);
+      // const session = await stripe.balanceTransactions.retrieve(req.query.key);
+      const session = await stripe.balanceTransactions.list();
+      console.log(
+        "🚀 ~ file: balanceTransactions.js ~ line 20 ~ handler ~ session",
+        session
+      );
+
       res.status(200).json({ session });
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
