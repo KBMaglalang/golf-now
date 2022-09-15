083a39b346ee840967366f28f1e117ca7f3a8028
Kurt Maglalang <kbm126@live.com>
2022-09-14 22:18:14 -0700

    comment out unnecessary routes in prisma user

--- pages/api/prisma/user.js
+++ pages/api/prisma/user.js
@@ -1,50 +1,51 @@
 import prisma from "../../../lib/prisma";
 
 export default async function handle(req, res) {
-  if (req.method === "POST") {
-    // try {
-    //   // const {
-    //   //   first_name,
-    //   //   last_name,
-    //   //   address_1,
-    //   //   city,
-    //   //   postal_code,
-    //   //   country,
-    //   //   telephone,
-    //   //   email,
-    //   //   role,
-    //   // } = req.body;
+  // if (req.method === "POST") {
+  //   // try {
+  //   //   // const {
+  //   //   //   first_name,
+  //   //   //   last_name,
+  //   //   //   address_1,
+  //   //   //   city,
+  //   //   //   postal_code,
+  //   //   //   country,
+  //   //   //   telephone,
+  //   //   //   email,
+  //   //   //   role,
+  //   //   // } = req.body;
 
-    //   const result = await prisma.user.create({
-    //     data: {
-    //       name: "asdf",
-    //       email: "asdf@asdf.com",
-    //       image: "asdf",
-    //       phoneNumber: "asdf",
-    //       country: "asdf",
-    //       city: "asdf",
-    //       postalCode: "asdf",
-    //       address1: "asdf",
-    //       address2: "asdf",
-    //     },
-    //   });
-    //   res.json(result);
-    // } catch (err) {
-    // }
-    res.status(err.statusCode || 500).json(err.message);
-  } else if (req.method === "GET") {
-    // try {
-    //   const result = await prisma.user.findUnique({
-    //     where: {
-    //       email: "asdf@asdf.com",
-    //     },
-    //   });
+  //   //   const result = await prisma.user.create({
+  //   //     data: {
+  //   //       name: "asdf",
+  //   //       email: "asdf@asdf.com",
+  //   //       image: "asdf",
+  //   //       phoneNumber: "asdf",
+  //   //       country: "asdf",
+  //   //       city: "asdf",
+  //   //       postalCode: "asdf",
+  //   //       address1: "asdf",
+  //   //       address2: "asdf",
+  //   //     },
+  //   //   });
+  //   //   res.json(result);
+  //   // } catch (err) {
+  //   // }
+  //   res.status(err.statusCode || 500).json(err.message);
+  // } else if (req.method === "GET") {
+  //   // try {
+  //   //   const result = await prisma.user.findUnique({
+  //   //     where: {
+  //   //       email: "asdf@asdf.com",
+  //   //     },
+  //   //   });
 
-    //   res.json(result);
-    // } catch (err) {
-    // }
-    res.status(err.statusCode || 500).json(err.message);
-  } else if (req.method === "PUT") {
+  //   //   res.json(result);
+  //   // } catch (err) {
+  //   // }
+  //   res.status(err.statusCode || 500).json(err.message);
+  // } else
+  if (req.method === "PUT") {
     try {
       const result = await prisma.user.update({
         where: {
@@ -59,20 +60,20 @@ export default async function handle(req, res) {
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
-  } else if (req.method === "DELETE") {
-    // try {
-    //   const result = await prisma.user.delete({
-    //     where: {
-    //       email: "asdf@asdf.com",
-    //     },
-    //   });
+    // } else if (req.method === "DELETE") {
+    //   // try {
+    //   //   const result = await prisma.user.delete({
+    //   //     where: {
+    //   //       email: "asdf@asdf.com",
+    //   //     },
+    //   //   });
 
-    //   res.json(result);
-    // } catch (err) {
-    // }
-    res.status(err.statusCode || 500).json(err.message);
+    //   //   res.json(result);
+    //   // } catch (err) {
+    //   // }
+    //   res.status(err.statusCode || 500).json(err.message);
   } else {
-    res.setHeader("Allow", "POST");
+    res.setHeader("Allow", "PUT");
     res.status(405).end("Method Not Allowed");
   }
 }
