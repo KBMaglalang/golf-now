69cff9f7be20a47d795258eb1e5768d9d716063a
Kurt Maglalang <kbm126@live.com>
2022-09-16 16:12:29 -0700

    clean up prisma order api

--- pages/api/prisma/order.js
+++ pages/api/prisma/order.js
@@ -21,65 +21,8 @@ export default async function handle(req, res) {
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
-  } else if (req.method === "GET") {
-    try {
-      const result = await prisma.order.findMany({
-        where: {
-          userId: "cl7r8kaqb00064mhdkw5eifzc",
-        },
-      });
-
-      res.json(result);
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
   }
 }
-
-// const createOrder = async () => {
-//   try {
-//     await fetch(`/api/prisma/order/`, {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       // body: JSON.stringify(undefined),
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-// const findOrder = async () => {
-//   try {
-//     await fetch(`/api/prisma/order/`, {
-//       method: "GET",
-//       headers: { "Content-Type": "application/json" },
-//       // body: JSON.stringify(undefined),
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-// const updateOrder = async () => {
-//   try {
-//     await fetch(`/api/prisma/order/`, {
-//       method: "PUT",
-//       headers: { "Content-Type": "application/json" },
-//       // body: JSON.stringify(undefined),
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-// const deleteOrder = async () => {
-//   try {
-//     await fetch(`/api/prisma/order/`, {
-//       method: "DELETE",
-//       headers: { "Content-Type": "application/json" },
-//       // body: JSON.stringify(undefined),
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
