10a266edb670b3c426ed020517a680e46ac5ea73
Kurt Maglalang <kbm126@live.com>
2022-09-19 13:57:13 -0700

    implement css on cancel and success page

--- pages/canceled.jsx
+++ pages/canceled.jsx
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Link from "next/link";
-import { runFireworks } from "../lib/fireworks";
+import styles from "../styles/Home.module.css";
+import payStyles from "../styles/Payment.module.css";
+import Head from "next/head";
 
 const Canceled = () => {
-  useEffect(() => {
-    runFireworks();
-  }, []);
-
   return (
-    <div className="cancel-wrapper">
-      <div className="cancel">
-        <h2>Forgot to add something to your cart?</h2>
-        <Link href="/">
-          <button type="button" width="300px" className="btn">
-            Return Home
-          </button>
-        </Link>
-      </div>
+    <div className={styles.container}>
+      <Head>
+        <title>Golf Now - Payment Canceled</title>
+        <meta name="description" content="Golf Products" />
+        <link rel="icon" href="/golf-ball-icon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={payStyles.wrapper}>
+          <div className={payStyles.cancel}>
+            <h2>Forgot to add something to your cart?</h2>
+          </div>
+          <Link href="/">
+            <button type="button" width="300px" className={payStyles.btn}>
+              Return Home
+            </button>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 };
