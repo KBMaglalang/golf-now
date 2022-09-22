dfa0db3323ad4993239884b6d06d7e256e1d9aeb
Kurt Maglalang <kbm126@live.com>
2022-09-22 15:25:37 -0700

    api test setup

--- pages/api/prisma/favorite.js
+++ pages/api/prisma/favorite.js
@@ -3,45 +3,57 @@ import prisma from "../../../lib/prisma";
 export default async function handle(req, res) {
   if (req.method === "POST") {
     try {
-      const { stripeData, cartItems, userData } = req.body;
-
-      const result = await prisma.order.create({
-        data: {
-          stripeOrderId: stripeData.session.id,
-          quantity: cartItems.quantity,
-          userId: userData.id,
-          status: "Pending",
-          productSKU: cartItems.sku,
-          productSubTotal: parseInt((cartItems.price * 100).toFixed(0)),
-          productName: cartItems.name,
-        },
-      });
-
-      res.json(result);
+      console.log(
+        "🚀 ~ file: favorite.js ~ line 7 ~ handle ~ req.body",
+        req.body
+      );
+      const { testString } = req.body;
+      console.log(
+        "🚀 ~ file: favorite.js ~ line 7 ~ handle ~ testString",
+        testString
+      );
+      // const result = await prisma.order.create({
+      //   data: {
+      //     stripeOrderId: stripeData.session.id,
+      //     quantity: cartItems.quantity,
+      //     userId: userData.id,
+      //     status: "Pending",
+      //     productSKU: cartItems.sku,
+      //     productSubTotal: parseInt((cartItems.price * 100).toFixed(0)),
+      //     productName: cartItems.name,
+      //   },
+      // });
+      // res.json(result);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
   } else if (req.method === "GET") {
     try {
-      const result = await prisma.user.findUnique({
-        where: {
-          email: req.query.key,
-        },
-      });
-
-      res.json(result);
+      console.log(
+        "🚀 ~ file: favorite.js ~ line 32 ~ handle ~ req.query.key",
+        req.query.key
+      );
+      // const result = await prisma.user.findUnique({
+      //   where: {
+      //     email: req.query.key,
+      //   },
+      // });
+      // res.json(result);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
   } else if (req.method === "PUT") {
     try {
-      const result = await prisma.user.findUnique({
-        where: {
-          email: req.query.key,
-        },
-      });
-
-      res.json(result);
+      console.log(
+        "🚀 ~ file: favorite.js ~ line 48  ~ handle ~ req.body",
+        req.body
+      );
+      // const result = await prisma.user.findUnique({
+      //   where: {
+      //     email: req.query.key,
+      //   },
+      // });
+      // res.json(result);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
