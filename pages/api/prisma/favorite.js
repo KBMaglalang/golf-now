84a35cbe5706707bcfb413f0f74769dba8e5d309
Kurt Maglalang <kbm126@live.com>
2022-09-22 17:12:07 -0700

    add delete api route

--- pages/api/prisma/favorite.js
+++ pages/api/prisma/favorite.js
@@ -62,22 +62,20 @@ export default async function handle(req, res) {
         "🚀 ~ file: favorite.js ~ line 62 ~ handle ~ req.body",
         req.body
       );
-      // const { product, prismaUserData } = req.body;
-      // console.log(
-      //   "🚀 ~ file: favorite.js ~ line 46 ~ handle ~ prismaUserData",
-      //   prismaUserData
-      // );
-      // console.log(
-      //   "🚀 ~ file: favorite.js ~ line 46 ~ handle ~ product",
-      //   product
-      // );
+      const { product, prismaUserData } = req.body;
+      console.log(
+        "🚀 ~ file: favorite.js ~ line 46 ~ handle ~ prismaUserData",
+        prismaUserData
+      );
+      console.log(
+        "🚀 ~ file: favorite.js ~ line 46 ~ handle ~ product",
+        product
+      );
 
       const result = await prisma.favorite.deleteMany({
         where: {
-          userId: "cl7r8kaqb00064mhdkw5eifzc",
-          productSanityId: "09ea3f50-673e-4c00-9313-c1f8c2e51370",
-          // userId: prismaUserData.id,
-          // productSanityId: product._id,
+          userId: prismaUserData.id,
+          productSanityId: product._id,
         },
       });
       console.log("🚀 ~ file: favorite.js ~ line 61 ~ handle ~ result", result);
@@ -86,7 +84,7 @@ export default async function handle(req, res) {
       res.status(err.statusCode || 500).json(err.message);
     }
   } else {
-    res.setHeader("Allow", "POST, GET, PUT");
+    res.setHeader("Allow", "POST, GET, PUT, DELETE");
     res.status(405).end("Method Not Allowed");
   }
 }
