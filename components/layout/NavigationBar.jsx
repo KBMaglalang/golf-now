91203889ad61ca3cbe6bc05ec7842879a62cd4df
Kurt Maglalang <kbm126@live.com>
2022-09-23 22:53:57 -0700

    basic material ui implementation of navbar

--- components/layout/NavigationBar.jsx
+++ components/layout/NavigationBar.jsx
@@ -1,10 +1,79 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { AiOutlineRight } from "react-icons/ai";
 import styles from "./NavigationBar.module.css";
+
+import {
+  Typography,
+  Grid,
+  Toolbar,
+  AppBar,
+  Container,
+  TextField,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+
+const ProfileMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <PersonOutlineOutlinedIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link href="/account">Account</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="/orders">Orders</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href="/favorites">Favorites</Link>
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            signOut();
+          }}
+        >
+          Logout
+        </MenuItem>
+      </Menu>
+    </Container>
+  );
+};
 
 export default function NavigationBar() {
   const { data: session } = useSession();
@@ -17,10 +86,73 @@ export default function NavigationBar() {
   };
 
   return (
-    <Fragment>
-      <header className={styles.header}>
+    <>
+      <AppBar position="relative" color="primary">
+        <Toolbar>
+          <Link href="/">
+            <Typography variant="h5" color="secondary">
+              Golf Now
+            </Typography>
+          </Link>
+          <Container maxWidth="lg">
+            <TextField
+              id="standard-basic"
+              label="Search"
+              variant="outlined"
+              sx={{ width: "50%" }}
+            />
+          </Container>
+
+          {session && <ProfileMenu />}
+          {!session && (
+            <IconButton aria-label="login" onClick={signIn}>
+              <LoginOutlinedIcon />
+            </IconButton>
+          )}
+
+          <Link href="/cart">
+            <IconButton aria-label="cart">
+              <ShoppingCartIcon />
+            </IconButton>
+          </Link>
+        </Toolbar>
+        <Toolbar>
+          <Button color="secondary" variant="text" href="/products/clubs">
+            Clubs
+          </Button>
+          <Button color="secondary" variant="text" href="/products/balls">
+            Balls
+          </Button>
+          <Button color="secondary" variant="text" href="/products/shoes">
+            Shoes
+          </Button>
+          <Button color="secondary" variant="text" href="/products/clothing">
+            Clothing
+          </Button>
+          <Button color="secondary" variant="text" href="/products/bag-carts">
+            {"Bags & Carts"}
+          </Button>
+          <Button color="secondary" variant="text" href="/products/golf-tech">
+            Golf Tech
+          </Button>
+          <Button color="secondary" variant="text" href="/products/brand">
+            Brand
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
+
+/*
+
+<header className={styles.header}>
         <div className={styles.logo}>
-          <Link href="/">Golf Now</Link>
+          <Link href="/">
+            <Typography variant="h5" color="primary">
+              Golf Now
+            </Typography>
+          </Link>
         </div>
 
         <form className="searchForm" onSubmit={handleSearch} role="search">
@@ -67,7 +199,7 @@ export default function NavigationBar() {
             <li>
               <Link href="/cart">
                 <div>
-                  <AiOutlineShoppingCart className={styles.cart} alt="cart" />
+                  <AiOutlineShoppingCart className={styles.cart} " />
                 </div>
               </Link>
             </li>
@@ -101,6 +233,4 @@ export default function NavigationBar() {
           </ul>
         </nav>
       </div>
-    </Fragment>
-  );
-}
+*/
