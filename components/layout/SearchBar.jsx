ec6e735f49d2363051958b0bf31954069dc52992
Kurt Maglalang <kbm126@live.com>
2022-09-28 15:02:49 -0700

    maximize the width of the search input text field

--- components/layout/SearchBar.jsx
+++ components/layout/SearchBar.jsx
@@ -39,7 +39,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "20ch",
+      width: "100%",
     },
   },
 }));
