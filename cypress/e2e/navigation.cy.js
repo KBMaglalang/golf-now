bcc96689f8b9db9ac96b4487bb2d2b3279110e33
Kurt Maglalang <kbm126@live.com>
2022-09-30 23:05:14 -0700

    update navigation cypress test

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -5,66 +5,65 @@ describe("Navigation", () => {
 
   it("should visit Homepage", () => {
     // The new page should contain an h1 with "About page"
-    cy.get("h1").contains("Top Selling Products");
+    cy.get("h3").contains("Top Selling Products");
   });
 
   it("should go to About page", () => {
     // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click();
+    cy.contains("p", "About Us").click();
 
     // The new url should include "/about"
     cy.url().should("include", "/about");
 
     // The new page should contain an h1 with "About page"
-    cy.get("h1").contains("About");
+    cy.get("h3").contains("About");
   });
 
   it("should go to CLUBS page", () => {
-    cy.contains("li", "Clubs").click();
+    cy.contains("a", "Clubs").click();
     cy.url().should("include", "/products/clubs");
-    cy.get("h1").contains("Clubs");
+    cy.get("h3").contains("Clubs");
   });
 
   it("should go to BALLS page", () => {
-    cy.contains("li", "Balls").click();
+    cy.contains("a", "Balls").click();
     cy.url().should("include", "/products/balls");
-    cy.get("h1").contains("Balls");
+    cy.get("h3").contains("Balls");
   });
 
   it("should go to SHOES page", () => {
-    cy.contains("li", "Shoes").click();
+    cy.contains("a", "Shoes").click();
     cy.url().should("include", "/products/shoes");
-    cy.get("h1").contains("Shoes");
+    cy.get("h3").contains("Shoes");
   });
 
   it("should go to CLOTHING page", () => {
-    cy.contains("li", "Clothing").click();
+    cy.contains("a", "Clothing").click();
     cy.url().should("include", "/products/clothing");
-    cy.get("h1").contains("Clothing");
+    cy.get("h3").contains("Clothing");
   });
 
   it("should go to BAGS AND CARTS page", () => {
-    cy.contains("li", "Bags & Carts").click();
+    cy.contains("a", "Bags & Carts").click();
     cy.url().should("include", "/products/bag-carts");
-    cy.get("h1").contains("Bag-carts");
+    cy.get("h3").contains("Bag Carts");
   });
 
   it("should go to GOLF TECH page", () => {
-    cy.contains("li", "Golf Tech").click();
+    cy.contains("a", "Golf Tech").click();
     cy.url().should("include", "/products/golf-tech");
-    cy.get("h1").contains("Golf-tech");
+    cy.get("h3").contains("Golf Tech");
   });
 
   it("should go to BRAND page", () => {
-    cy.contains("li", "Brand").click();
+    cy.contains("a", "Brand").click();
     cy.url().should("include", "/products/brand");
-    cy.get("h1").contains("Brand");
+    cy.get("h3").contains("Brand");
   });
 
   it("should go to CART page", () => {
-    cy.get('[alt="cart"]').click();
+    cy.get('[aria-label="cart"]').click();
     cy.url().should("include", "/cart");
-    cy.get("h1").contains("Your Cart");
+    cy.get("h3").contains("Your Cart");
   });
-  // it ("should go to SPECIFIC PRODUCT page", () => {});
 });
