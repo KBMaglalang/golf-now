bb05ad99f1f0a3b0ce17efb5cd64727c9e4eba29
Kurt Maglalang <kbm126@live.com>
2022-09-30 23:27:41 -0700

    update cypress tests to work with material ui

--- cypress/e2e/order.cy.js
+++ cypress/e2e/order.cy.js
@@ -5,38 +5,38 @@ describe("Order", () => {
   });
 
   it("should have nothing in the cart", () => {
-    cy.get('[alt="cart"]').click();
+    cy.get('[aria-label="cart"]').click();
     cy.url().should("include", "/cart");
-    cy.get("span").contains("Cart is Empty");
+    cy.get("main").contains("Cart is Empty");
   });
 
   it("should add product to cart", () => {
     cy.clearLocalStorage();
     cy.visit("/");
-    cy.contains("Under Armour").click();
-    cy.get("h4").should("contain", "40010826");
+    cy.contains("Rogue ST Max Driver").click();
+    cy.get("h6").should("contain", "40128761");
 
-    cy.get("button").last().click();
+    cy.contains("Add to Cart").click();
 
-    cy.get('[alt="cart"]').click();
+    cy.get('[aria-label="cart"]').click();
     cy.url().should("include", "/cart");
-    cy.get("span").contains("Men's Playoff 2.0 Short Sleeve Polo");
+    cy.get("main").contains("Rogue ST Max Driver");
   });
 
   it("should not be able to add to cart", () => {
     cy.clearLocalStorage();
 
     cy.visit("/");
-    cy.contains("li", "Balls").click();
+    cy.contains("a", "Balls").click();
     cy.url().should("include", "/products/balls");
 
     cy.contains("2022 Tour Speed Golf Balls").click();
-    cy.get("h4").should("contain", "4012846101");
+    cy.get("h6").should("contain", "4012846101");
 
-    cy.get("button").last().should("be.disabled");
+    cy.contains("Add to Cart").should("be.disabled");
 
-    cy.get('[alt="cart"]').click();
+    cy.get('[aria-label="cart"]').click();
     cy.url().should("include", "/cart");
-    cy.get("span").should("not.contain", "2022 Tour Speed Golf Balls");
+    cy.get("main").should("not.contain", "2022 Tour Speed Golf Balls");
   });
 });
