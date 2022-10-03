64d5f6da83954ed2ba4bd7275fa32bf049858715
Kurt Maglalang <kbm126@live.com>
2022-10-03 11:00:14 -0700

    remove styles from layout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import NavigationBar from "./NavigationBar";
 import FooterBar from "./Footer";
-import styles from "./Layout.module.css";
 
 export default function Layout(props) {
   return (
