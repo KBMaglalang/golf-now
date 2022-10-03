a3609d10685e070aa8d7d8ff0473c311129bc816
Kurt Maglalang <kbm126@live.com>
2022-10-03 10:34:43 -0700

    remove css module from layout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -7,8 +7,8 @@ export default function Layout(props) {
   return (
     <Fragment>
       <NavigationBar />
-      <main className={styles.main}>{props.children}</main>
-      <footer className={styles.footer}>
+      <main>{props.children}</main>
+      <footer>
         <FooterBar />
       </footer>
     </Fragment>
