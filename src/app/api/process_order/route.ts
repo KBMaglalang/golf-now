9776fc5bd5b28304a10022e578f43de3345822e9
Kurt Maglalang <kbm126@live.com>
2023-12-03 17:25:48 -0800

    feat: create route to handle orders that are paid

---
+++ src/app/api/process_order/route.ts
@@ -0,0 +1,61 @@
+import { NextResponse, NextRequest } from "next/server";
+
+// components
+
+// context or store
+
+// constants and functions
+import { prisma } from "@/config/prisma/prisma";
+import { client as sanityClient } from "@/lib/sanity";
+import { stripe } from "@/config/stripe/stripe.server";
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const { stripeId, cart, session } = data;
+
+  if (!stripeId || !cart || !session) {
+    return new NextResponse("Missing Arguments", { status: 400 });
+  }
+
+  try {
+    // get order details
+    const orderDetails = await stripe.checkout.sessions.retrieve(stripeId);
+    if (!orderDetails || orderDetails.payment_status !== "paid")
+      return new NextResponse("No Changes", { status: 200 });
+
+    // get prisma user details
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user?.email!,
+      },
+    });
+    if (!user) return new NextResponse("Missing Cart", { status: 400 });
+
+    // update the orders in prisma
+    await prisma.order.updateMany({
+      where: {
+        stripeOrderId: stripeId,
+        userId: user.id,
+      },
+      data: {
+        status: "Paid",
+      },
+    });
+
+    // update the sanity stock for each product
+    await Promise.all(
+      cart.map(async (product: any) => {
+        await sanityClient
+          .patch(product._id)
+          .dec({ stock: product.quantity })
+          .commit();
+      })
+    );
+
+    return new NextResponse(JSON.stringify("Order Updated"), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse(error, {
+      status: 400,
+    });
+  }
+}
