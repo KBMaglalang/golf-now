4fc011b1b46de11f5c9c1d30ae3d1879a71f9e68
Kurt Maglalang <kbm126@live.com>
2023-12-03 17:26:45 -0800

    feat: update success page for completed orders

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from "react";
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
-import type { User } from "@prisma/client";
 import { Session } from "next-auth";
 
 // components
@@ -15,114 +14,38 @@ import { useStateContext } from "@/context/stateContext";
 
 // constants and functions
 
-/**
-
-Retrieves order information from the server using Stripe API.
-@param {string} id - The ID of the order to retrieve information for.
-@returns {Promise<Response>} - A promise that resolves to the response from the server. */
-const stripeGetOrderInfo = async (id: string) => {
-  return await fetch(`/api/checkout_sessions?key=${id}`, {
-    method: "GET",
+const processData = async (
+  cart: CartItemType[],
+  stripeId: string,
+  session: Session
+) => {
+  // get stripe checkout information
+  const orderResponse = await fetch(`/api/process_order`, {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-  });
-};
-
-/**
-
-Retrieves user information from the server using Prisma API.
-@param {Session} session - The session object containing user information.
-@returns {Promise<Response>} - A promise that resolves to the response from the server. */
-const prismaGetUserInfo = async (session: Session) => {
-  return await fetch(`/api/user/profile?key=${session?.user?.email}`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
-};
-
-/**
-
-Creates a new order by sending a POST request to the server using Prisma API.
-@param {CartItemType} product - The product item to be included in the order.
-@param {any} stripeData - Data related to the Stripe payment for the order.
-@param {User} prismaUserData - User data to be associated with the order.
-@returns {Promise<Response>} - A promise that resolves to the response from the server. */
-const prismaCreateOrder = async (
-  product: CartItemType,
-  stripeData: any,
-  prismaUserData: User
-) => {
-  return await fetch(`/api/order/`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      cartItems: product,
-      stripeData,
-      userDetails: prismaUserData,
+      stripeId,
+      cart,
+      session,
     }),
   });
+  if (orderResponse.ok === false) return;
+  const response = await orderResponse.json();
 };
-
-/**
-
-Updates the stock of products in the server using Sanity API.
-@param {CartItemType[]} cartDetails - An array of cart items containing product details.
-@returns {Promise<Response>} - A promise that resolves to the response from the server. */
-const sanityUpdateProductStock = async (cartDetails: CartItemType[]) => {
-  return await fetch("/api/sanity", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ cartDetails }),
-  });
-};
-
 export default function Page() {
   const { cart, clearCart } = useStateContext();
   const stripeId = useSearchParams().get("session_id");
   const { data: session } = useSession();
 
-  // const processData = async () => {
-  //   if (!stripeId) return;
-  //   if (!session) return;
+  useEffect(() => {
+    if (!stripeId || !session || !cart) return;
 
-  //   // get stripe checkout information
-  //   const stripeResponse = await stripeGetOrderInfo(stripeId);
-  //   if (stripeResponse.ok === false) return;
-  //   const stripeData = await stripeResponse.json();
-
-  //   if (stripeData?.payment_status === "paid") {
-  //     if (cart.length) {
-  //       // get information about the logged in user
-  //       const response = await prismaGetUserInfo(session);
-  //       if (response.ok === false) return;
-  //       const prismaUserData = await response.json();
-
-  //       // store the data in prisma
-  //       await Promise.all(
-  //         cart.map(async (product) => {
-  //           prismaCreateOrder(product, stripeData, prismaUserData);
-  //         })
-  //       );
-
-  //       // update the stock amount in sanity
-  //       const sanityCheck = await sanityUpdateProductStock(cart);
-  //       if (sanityCheck.ok === false) return;
-  //       await sanityCheck.json();
-
-  //       // clear out cart items
-  //       clearCart();
-  //       localStorage.clear();
-  //     }
-  //   }
-  // };
-
-  // clear the cart
-  // useEffect(() => {
-  //   processData();
-  // }, []);
+    processData(cart, stripeId, session);
+    clearCart();
+    localStorage.clear();
+  }, [session, stripeId]);
 
   return (
     <main className="container mx-auto my-12 flex-1 p-5">
