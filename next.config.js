921dc299367eb279714ea2b4ffb4ea86b8e9d1d7
Kurt Maglalang <kbm126@live.com>
2023-12-19 14:34:09 -0800

    fix: update domains to remote patterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "daisyui.com", "cdn.sanity.io"],
+    // domains: ["images.unsplash.com", "daisyui.com", "cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "daisyui.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
   reactStrictMode: false,
 };
